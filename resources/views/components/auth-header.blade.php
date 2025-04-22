6b0810039e86f0983d58633fd975680615434e28
boy indi prasetya <boyindi1905@gmail.com>
2025-04-23 05:13:55 +0700

    fortify add login

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,6 +1,10 @@
- <div class="login-brand">
-     <img src="{{ asset('img/stisla-fill.svg') }}"
-         alt="logo"
-         width="100"
-         class="shadow-light rounded-circle">
- </div>
+<div class="login-brand mb-4" style="margin-top: -20px;">
+    <img src="{{ asset('img/regware-logo.png') }}" alt="logo"
+         style="width: 150px; border-radius: 50%; padding: 10px; background-color: #ffffff;"
+         class="shadow-light">
+</div>
+
+
+
+{{-- <div class="login-brand mb-4" style="margin-top: -20px;">
+    <h4 class="text-center">Regware</h4> --}}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function PHPSTORM_META\type;
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', function () {
+        return view('pages.dashboard', [
+            'type_menu' => 'dashboard'
+        ]);
+    })->name('home');
+
+
 });
